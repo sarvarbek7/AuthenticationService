fd89c1b6e1e6dd3033cc24e896e807c71d7f711f
Sarvarbek Bakhtiyorov <bakhtiyorov.sarvar2001@gmail.com>
2023-03-18 12:01:51 +0500

    DATA: Add User Model

---
+++ AuthenticationService.Api/Models/Users/Profession.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AuthenticationService.Api.Models.Users
+{
+    public class Profession : IdentityRole<Guid>
+    {
+    }
+}
---
+++ AuthenticationService.Api/Models/Users/User.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthenticationService.Api.Models.Users
+{
+    [Index(nameof(PhoneNumber), IsUnique = true)]
+    public class User : IdentityUser<Guid>
+    {
+        public override Guid Id
+        {
+            get => base.Id;
+            set => base.Id = value;
+        }
+
+        [Required]
+        public override string PhoneNumber
+        {
+            get => base.PhoneNumber;
+            set => base.PhoneNumber = value;
+        }
+
+        public override string? Email
+        {
+            get => base.Email;
+            set => base.Email = value;
+        }
+
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Mosque { get; set; }
+        public Gender Gender { get; set; }
+        public string? ProfilePhotoUrl { get; set; }
+        public DateTimeOffset? BirthDate { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+
+    }
+}
