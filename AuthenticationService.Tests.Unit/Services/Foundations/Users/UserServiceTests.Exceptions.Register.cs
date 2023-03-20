9c8ad753505047980a8255ccc5809f8c92314505
Sarvarbek Bakhtiyorov <bakhtiyorov.sarvar2001@gmail.com>
2023-03-20 12:28:40 +0500

    ShouldThrowUserDependencyValidationExceptionOnRegisterIfDuplicateKeyExceptionOccursAndLogItAsync -> PASS

--- AuthenticationService.Tests.Unit/Services/Foundations/Users/UserServiceTests.Exceptions.Register.cs
+++ AuthenticationService.Tests.Unit/Services/Foundations/Users/UserServiceTests.Exceptions.Register.cs
@@ -53,5 +53,50 @@ namespace AuthenticationService.Tests.Unit.Services.Foundations.Users
             this.userManagement.VerifyNoOtherCalls();
             this.loggingBroker.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async void ShouldThrowUserDependencyValidationExceptionOnRegisterIfDuplicateKeyExceptionOccursAndLogItAsync()
+        {
+            // given
+            string uzbPhoneNumberFormat = @"^\+998[9873][01345789][0-9]{7}$";
+            var xeger = new Xeger(uzbPhoneNumberFormat);
+            var generatedPhoneNumber = xeger.Generate();
+            User randomUser = CreateRandomUser();
+            randomUser.PhoneNumber = generatedPhoneNumber;
+            User inputUser = randomUser;
+            string roleName = CreateRandomRole();
+            string randomMessage = GetRandomMessage();
+
+            var duplicateKeyException =
+                new DuplicateKeyException(randomMessage);
+
+            var alreadyExistsUserException = new AlreadyExistsUserException(
+                duplicateKeyException);
+
+            var expectedUserDependencyValidationException =
+                new UserDependencyValidationException(alreadyExistsUserException);
+
+            this.userManagement.Setup(broker =>
+                broker.InsertUserAsync(inputUser, roleName)).ThrowsAsync(duplicateKeyException);
+
+            // when
+            ValueTask<User> registerUserTask =
+                this.userService.RegisterUserAsync(inputUser, roleName);
+
+            // then
+            await Assert.ThrowsAsync<UserDependencyValidationException>(() =>
+                registerUserTask.AsTask());
+
+            this.userManagement.Verify(broker =>
+                broker.InsertUserAsync(inputUser, roleName),
+                Times.Once);
+
+            this.loggingBroker.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedUserDependencyValidationException))),
+                Times.Once);
+
+            this.userManagement.VerifyNoOtherCalls();
+            this.loggingBroker.VerifyNoOtherCalls();
+        }
     }
 }
